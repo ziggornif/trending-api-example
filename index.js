eb1aa48720d9ed59a818b85fb47796aa16320bbe
drouian-m <drouian-m@mgdis.fr>
2019-09-25 22:26:10 +0200

    feat: add description
    add linter

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function callGithubTrending(language, since = 'today') {
     const page = await browser.newPage();
     const navigationPromise = page.waitForNavigation();
     await page.setRequestInterception(true);
-    page.on('request', (req) => {
+    page.on('request', req => {
       if (req.resourceType() === 'document') {
         req.continue();
       } else {
@@ -17,18 +17,21 @@ async function callGithubTrending(language, since = 'today') {
       }
     });
     await page.goto(`https://github.com/trending/${language}?since=${since}`);
-
-
-
     await navigationPromise;
     const elements = await page.$$('.Box > div > .Box-row');
 
-    for (var element of elements) {
-      const project = await element.$eval('.h3', s => s.textContent.replace(/(\r\n|\n|\r|\s)/gm, "").trim());
+    for (const element of elements) {
+      const project = await element.$eval('.h3', s =>
+        s.textContent.replace(/(\r\n|\n|\r|\s)/gm, '').trim(),
+      );
+      const description = await element.$eval('.col-9', d =>
+        d.textContent.trim(),
+      );
       const url = await element.$eval('.h3 > a', a => a.href);
       results.push({
         project,
-        url
+        description,
+        url,
       });
     }
 
@@ -40,7 +43,6 @@ async function callGithubTrending(language, since = 'today') {
       projects: results,
     };
   } catch (error) {
-    console.log(error)
     throw Error('Github call error');
   }
 }
@@ -49,48 +51,51 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.redirect('/api');
-})
+});
 
 app.get('/api', (req, res) => {
-  res.json([{
+  res.json([
+    {
       url: 'http://localhost:3000/api/trending',
-      title: 'Trending root page'
+      title: 'Trending root page',
     },
     {
       url: 'http://localhost:3000/api/trending/:language',
-      title: 'Trending language page'
-    }
-  ])
-})
+      title: 'Trending language page',
+    },
+  ]);
+});
 
 app.get('/api/trending', async (req, res) => {
   res.json({
     languages: {
-      values: [
-        'javascript', 'python', 'css', 'typescript', 'go', '...'
-      ],
+      values: ['javascript', 'python', 'css', 'typescript', 'go', '...'],
       example: 'http://localhost:3000/api/trending/javascript',
     },
     since: {
-      values: [
-        'daily', 'weekly (default value)', 'monthly'
-      ],
+      values: ['daily', 'weekly (default value)', 'monthly'],
       example: 'http://localhost:3000/api/trending/javascript?daily',
-    }
+    },
   });
 });
 
 app.get('/api/trending/:language', async (req, res) => {
-  const results = await callGithubTrending(req.params.language, req.query.since);
+  const results = await callGithubTrending(
+    req.params.language,
+    req.query.since,
+  );
   res.json(results);
 });
 
-app.listen({
-  port: 3000
-}, async () => {
-  browser = await puppeteer.launch();
-  console.log(`🚀 Server ready at http://localhost:3000`);
-});
+app.listen(
+  {
+    port: 3000,
+  },
+  async () => {
+    browser = await puppeteer.launch();
+    console.log('🚀 Server ready at http://localhost:3000');
+  },
+);
 
 process.on('SIGTERM', () => {
   browser.close();
